d051ca6e608927ae203f5cb7f83aeb7b266544f9
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-09 12:23:19 +0200

    initializeDB removed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import { initializeDB } from "./database/services/initializeDB";
 import { App } from "./app.global";
 
 const app = new App();
 
-    (async () => {
-        await initializeDB();
-        app.listen();
-    })();
+(() => {
+    app.listen();
+})();
