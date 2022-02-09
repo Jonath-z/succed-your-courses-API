70c2183df83b731821813871353b5ee369d0eed4
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-09 14:55:15 +0200

    default route added

--- src/app.global.ts
+++ src/app.global.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { routes } from './routes';
+import { Request, Response } from 'express';
 
 export class App{
     public app: express.Application;
@@ -11,6 +12,7 @@ export class App{
         this.app = express();
         this.initilizeMiddleware();
         this.initializeRoutes(routes);
+        this.initializeDefaultRoutes();
     }
 
     private initilizeMiddleware(): void{
@@ -22,6 +24,13 @@ export class App{
     private initializeRoutes(routes: any[]): void {
         routes.forEach(route => {
             this.app.use('/api/v1', route.router);
+        });
+    }
+    private initializeDefaultRoutes(): void {
+        this.app.get('/', (req: Request, res: Response) => {
+            return res.json({
+                message: `Welcome to succed your course API`,
+            });
         });
     }
 
